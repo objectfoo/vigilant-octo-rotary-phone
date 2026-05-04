8bbcc539098a3cb9b69fad651ac528fcda667b1c
objectfoo <objectfoo@users.noreply.github.com>
2026-05-04 14:27:27 -0700

    scaffold minimal app

---
+++ src/Index/Index.tsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function Index() {
+	return (
+		<Box sx={{ m: 3 }}>
+			<Typography variant="h1">Hello</Typography>
+			<Typography variant="subtitle1">world</Typography>
+		</Box>
+	);
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import "./main.css";
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
+import { createBrowserRouter } from "react-router";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router/dom";
+import { StrictMode } from "react";
+import Index from "./Index/Index";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+]);
+
+
+createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
